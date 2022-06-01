76c4feb43335d21fbc583886261c3309b5448711
Leandro Morales <leomoralesarce@gmail.com>
2022-06-01 00:42:31 -0300

    compress images

--- constants/projects.js
+++ constants/projects.js
@@ -2,35 +2,35 @@ export const projects = [
   {
     name: 'Puppy Point',
     desc: "Puppy Point is an application created with the PERN stack that connects dog walkers with people who do not have time and want their pets to be walked.",
-    img: '/images/projects/puppy-point.png',
+    img: '/images/projects/puppy-point.webp',
     url: 'https://puppypoint.vercel.app/',
     github: "https://github.com/lemorles/puppy-point"
   },
   {
     name: 'Seguros Contar',
     desc: "Seguros Contar is a SPA (Single Page Application) in which you can view the vehicle insurance quotes in real time from the company Galeno Seguros.",
-    img: '/images/projects/seguros-contar.png',
+    img: '/images/projects/seguros-contar.webp',
     url: 'https://cotizar.seguroscontar.com.ar/',
     github: "https://github.com/lemorles/seguros-contar"
   },
   {
     name: 'Countries App',
     desc: "Countries App is a SPA (Single Page Application) in which all the countries of the world and their respective tourist activities can be viewed.",
-    img: '/images/projects/countries.png',
+    img: '/images/projects/countries.webp',
     url: 'https://pi-countries-bay.vercel.app/',
     github: "https://github.com/lemorles/countries-app"
   },
   {
     name: 'Portfolio',
     desc: "The portfolio is an app created with Next.js, a React.js framework, in which the skills and some of the projects carried out are shown.",
-    img: '/images/projects/portfolio.png',
+    img: '/images/projects/portfolio.webp',
     url: 'https://portfolio-leandro-morales.vercel.app/',
     github: "https://github.com/lemorles/porfolio"
   },
   {
     name: 'Weather App',
     desc: "Weather App is a SPA (Single Page Application) that consumes information from an API and displays the weather of the places chosen by the user.",
-    img: '/images/projects/weather.png',
+    img: '/images/projects/weather.webp',
     url: 'https://app-clima-delta.vercel.app/',
     github: "https://github.com/lemorles/weather-app"
   },
